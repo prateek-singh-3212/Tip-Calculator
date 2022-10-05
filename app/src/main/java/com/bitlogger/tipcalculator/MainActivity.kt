c93d373bb3d516dfebc0486e634222f6f88fee1e
Prateek Singh <prateeksingh3212@gmail.com>
2022-10-05 11:22:59 +0530

    Added Round Switch

--- app/src/main/java/com/bitlogger/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/bitlogger/tipcalculator/MainActivity.kt
@@ -5,17 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Switch
+import androidx.compose.material.SwitchDefaults
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlin.math.roundToLong
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,6 +41,10 @@ fun TipCalculator() {
     var inputAmount by remember {
         mutableStateOf("")
     }
+    val tipAmount = inputAmount.toFloatOrNull() ?: 0.0f
+    var isRound by remember {
+        mutableStateOf(false)
+    }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -54,9 +62,27 @@ fun TipCalculator() {
         EditTextField("Enter Amount", inputAmount) {
             inputAmount = it
         }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .size(48.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = "Round Value?")
+            Switch(
+                checked = isRound,
+                onCheckedChange = { isRound = it },
+                colors = SwitchDefaults.colors(
+                    uncheckedThumbColor = Color.DarkGray
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentWidth(Alignment.End)
+            )
+        }
         Spacer(modifier = Modifier.height(50.dp))
         Text(
-            text = "Tip is: ${calculateBasicTip(inputAmount.toFloatOrNull() ?: 0.0f)}",
+            text = "Tip is: ${calculateBasicTip(tipAmount, isRound)}",
             fontSize = 18.sp,
             modifier = Modifier.fillMaxWidth(),
             textAlign = TextAlign.Center
@@ -64,8 +90,13 @@ fun TipCalculator() {
     }
 }
 
-fun calculateBasicTip(amount: Float): Float {
-    return 0.15f * amount
+fun calculateBasicTip(amount: Float, isRound: Boolean): String {
+    val res = (0.15f * amount)
+    return if (isRound) {
+        res.roundToLong().toString()
+    } else {
+        res.toString()
+    }
 }
 
 @Composable
