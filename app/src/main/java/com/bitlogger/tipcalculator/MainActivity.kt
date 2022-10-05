c9d68da9889ab66258ad54df52ba698aaedc800c
Prateek Singh <prateeksingh3212@gmail.com>
2022-10-05 10:38:36 +0530

    Basic Tip Calculator UI

--- app/src/main/java/com/bitlogger/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/bitlogger/tipcalculator/MainActivity.kt
@@ -3,41 +3,74 @@ package com.bitlogger.tipcalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import com.bitlogger.tipcalculator.ui.theme.TipCalculatorTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TipCalculatorTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            TipCalculatorUI()
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TipCalculatorUI() {
+    TipCalculator()
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    TipCalculatorTheme {
-        Greeting("Android")
+fun TipCalculator() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.TopCenter)
+            .padding(20.dp)
+    ) {
+        Text(
+            text = "Basic Tip Calculator",
+            fontSize = 20.sp,
+            fontWeight = FontWeight(700),
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.height(50.dp))
+        EditTextField()
+        Spacer(modifier = Modifier.height(50.dp))
+        Text(
+            text = "Tip is: ",
+            fontSize = 18.sp,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
     }
+}
+
+@Composable
+fun EditTextField() {
+    var data by remember {
+        mutableStateOf("")
+    }
+    TextField(
+        value = data,
+        onValueChange = { data = it },
+        modifier = Modifier.fillMaxWidth(),
+        label = {
+            Text(text = "Enter Amount")
+        },
+        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number)
+    )
 }
